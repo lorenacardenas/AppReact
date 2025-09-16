460be834aeebc7ffa6fe739db1707533a33c75b1
Lorena Cardenas <lorenacs2000@gmail.com>
2025-09-16 17:34:19 -0500

    commit de archivos restantes

---
+++ index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import express from "express"
+
+const app = express()
+
+app.get('/',(req, res) => {
+    res.send('HOLA MUNDO')
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT,() => {
+    console.log("App escuchando en puerto:", PORT)
+})
